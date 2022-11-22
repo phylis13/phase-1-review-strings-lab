3b2bc30fd40d8ef7fed3eb00877b21277a11d35f
phylis13 <phylisnjeri54@gmail.com>
2022-11-22 15:55:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = `Grace Hopper`;
+let welcomeMessage = "Welcome to Flatbook, Grace Hopper!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, GRACE HOPPER!"
+ const shortGreeting = "Welcome, G!"
